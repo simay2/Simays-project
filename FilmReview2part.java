aaeba2830adf45edc2005393487f0ad8cbb4be86
simay2 <72138608+simay2@users.noreply.github.com>
2021-01-20 23:27:37 +0200

    Step 7 TODO -> DONE

--- FilmReview2part.java
+++ FilmReview2part.java
@@ -1,6 +1,7 @@
 package project;
 
   import java.awt.BorderLayout;
+  import java.awt.Container;
  import java.awt.event.ActionListener;
  import java.awt.Color;
 import java.awt.Font;
@@ -15,7 +16,8 @@ import java.awt.LayoutManager;
  import java.util.Date;
  import java.util.Scanner;
 
-  import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
  import javax.swing.ButtonGroup;
  import javax.swing.Icon;
  import javax.swing.JButton;
@@ -29,42 +31,42 @@ import java.awt.LayoutManager;
  import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.text.AbstractDocument.Content;
+import java.awt.Component;
+
 
   public class FilmReview2part {
 
 
   	public static void main(String[] args) {
  		// TODO Auto-generated method stub
+  	// main containers and Layouts
  		JFrame frame = new JFrame("Film Reviews");
- 		frame.setSize(800, 600);
  		frame.setBackground(Color.black);
  
   		JPanel mainPanel = new JPanel(); 
- 		frame.add(mainPanel);
+ 		frame.getContentPane().add(mainPanel);
  		mainPanel.setLayout((LayoutManager) new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+ 		mainPanel.setBackground(Color.WHITE);
  		
- 		JPanel TitlePanel = new JPanel(); 
- 		mainPanel.add(TitlePanel);
- 		TitlePanel.setLayout((LayoutManager) new BoxLayout(TitlePanel, BoxLayout.Y_AXIS));
- 		
-  		JPanel leftPanel = new JPanel();
+		JPanel leftPanel = new JPanel();
  		mainPanel.add(leftPanel);
  		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+ 		leftPanel.setBackground(Color.WHITE);	 
  		
- 		JLabel titleLabel = new JLabel("Film Review");
- 		TitlePanel.add(titleLabel);
- 		titleLabel.setForeground(Color.black);
- 		titleLabel.setFont(new Font("Brush Script MT", Font.BOLD, 24));
-
- 
- 
-  		JPanel rightPanel = new JPanel();
+ 		JPanel rightPanel = new JPanel();
  		mainPanel.add(rightPanel);
  		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+ 		rightPanel.setBackground(Color.WHITE);
+ 		
+ 		JLabel titleLabel  = new JLabel("Film Review");
+ 		titleLabel.setBackground(Color.white);
+ 		titleLabel.setOpaque(true);
+ 		titleLabel.setForeground(Color.pink);
+ 		leftPanel.add( titleLabel, "push, align center" );
+ 		titleLabel.setFont(new Font("Copperplate", Font.BOLD, 26 ));
 
-  		JPanel RatePanel = new JPanel(); 
- 		mainPanel.add(RatePanel);
- 		RatePanel.setLayout((LayoutManager) new BoxLayout(RatePanel, BoxLayout.X_AXIS));
  		
   		JTextArea criticArea = new JTextArea(15,10);
  		 frame.setVisible(true);
@@ -102,61 +104,76 @@ import javax.swing.SwingConstants;
  
   		SimpleDateFormat ft = new SimpleDateFormat("dd. M yyyy");
  		JLabel currentDateLabel = new JLabel(ft.format(new Date()));
+ 		currentDateLabel.setHorizontalAlignment(SwingConstants.CENTER);
+ 		
  		rightPanel.add(currentDateLabel);
+ 		
 
  
   		JTextField filmnamertext1 = new JTextField();
  		rightPanel.add(filmnamertext1);
  		JTextField criticrtext1 = new JTextField();
  		rightPanel.add(criticrtext1);
-
- 
-  		rightPanel.add(RatePanel);
-
-  		JRadioButton firstButton = new JRadioButton("*");
- 		 firstButton.setMnemonic(KeyEvent.VK_B);
- 		    firstButton.setActionCommand("*");
- 		    firstButton.setSelected(false);
- 		    firstButton.setVisible(true);
- 		    RatePanel.add(firstButton);
-
- 
+  		  		    
+  		JPanel RatePanel = new JPanel();
+  		RatePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+  		RatePanel.setBackground(Color.WHITE);
+ 		rightPanel.add(RatePanel);
+ 		RatePanel.setLayout((LayoutManager) new BoxLayout(RatePanel, BoxLayout.X_AXIS));
+  		  		 ButtonGroup group = new ButtonGroup();
+  		
+  
+  		  		
+  		
+  		  		JRadioButton firstButton = new JRadioButton("*");
+  		  	RatePanel.add(firstButton);
+  		  		firstButton.setMnemonic(KeyEvent.VK_B);
+  		  		firstButton.setActionCommand("*");
+  		  		firstButton.setSelected(false);
+  		  		firstButton.setVisible(true);
+  		  		group.add(firstButton);
+  		
+  		
+  		 
   		    JRadioButton secondButton = new JRadioButton("**");
- 		    secondButton.setMnemonic(KeyEvent.VK_B);
- 		    secondButton.setActionCommand("**");
- 		    secondButton.setSelected(false);
- 		    secondButton.setVisible(true);
- 		    RatePanel.add(secondButton);
-
-  		    JRadioButton thirtButton = new JRadioButton("***");
- 		    thirtButton.setMnemonic(KeyEvent.VK_B);
- 		    thirtButton.setActionCommand("***");
- 		    thirtButton.setSelected(false);
- 		    thirtButton.setVisible(true);
- 		    RatePanel.add(thirtButton);
-
-  		    JRadioButton forthButton = new JRadioButton("****");
- 		    forthButton.setMnemonic(KeyEvent.VK_B);
- 		    forthButton.setActionCommand("****");
- 		    forthButton.setSelected(false);
- 		    forthButton.setVisible(true);
- 		    RatePanel.add(forthButton);
-
-  		    JRadioButton fifthButton = new JRadioButton("*****");
- 		    fifthButton.setMnemonic(KeyEvent.VK_B);
- 		    fifthButton.setActionCommand("*****");
- 		    fifthButton.setSelected(false);
- 		    fifthButton.setVisible(true);
- 		    RatePanel.add(fifthButton);
-
-  		    ButtonGroup group = new ButtonGroup();
- 		    group.add(firstButton);
- 		    group.add(secondButton);
- 		    group.add(thirtButton);
- 		    group.add(forthButton);
- 		    group.add(fifthButton);
+  		    RatePanel.add(secondButton);
+  		    secondButton.setMnemonic(KeyEvent.VK_RIGHT);
+  		    secondButton.setActionCommand("**");
+  		    secondButton.setSelected(false);
+  		    secondButton.setVisible(true);
+  		    group.add(secondButton);
+  		
+  		
+  		JRadioButton forthButton = new JRadioButton("****");
+  		RatePanel.add(forthButton);
+  		forthButton.setMnemonic(KeyEvent.VK_B);
+  		forthButton.setActionCommand("****");
+  		forthButton.setSelected(false);
+  		forthButton.setVisible(true);
+  		group.add(forthButton);
+  		
+  		
+  		JRadioButton thirtButton = new JRadioButton("***");
+  		RatePanel.add(thirtButton);
+  		thirtButton.setMnemonic(KeyEvent.VK_B);
+  		thirtButton.setActionCommand("***");
+  		thirtButton.setSelected(false);
+  		thirtButton.setVisible(true);
+  		group.add(thirtButton);
+  		
+  		
+  		JRadioButton fifthButton = new JRadioButton("*****");
+  		RatePanel.add(fifthButton);
+  		fifthButton.setMnemonic(KeyEvent.VK_B);
+  		fifthButton.setActionCommand("*****");
+  		fifthButton.setSelected(false);
+  		fifthButton.setVisible(true);
+  		group.add(fifthButton);
 
   		JButton submitButton = new JButton("Submit");
+  		submitButton.setHorizontalAlignment(SwingConstants.LEADING);
+  		submitButton.setForeground(Color.pink);
+  		rightPanel.add( submitButton, "push, align center" );
  		submitButton.addActionListener(new ActionListener() {
 
   			public void actionPerformed(ActionEvent arg0) {
@@ -194,7 +211,6 @@ import javax.swing.SwingConstants;
   					try {
   						myObj.createNewFile();
   					} catch (IOException e1) {
-  						// TODO Auto-generated catch block
   						e1.printStackTrace();
   					}
   				}
@@ -204,15 +220,22 @@ import javax.swing.SwingConstants;
   		});
 
   		rightPanel.add(submitButton);
-
-    		JLabel inspirationLabel = new JLabel("Who doesn't hate to watch a bad move twice? "
- 				+ "If you are bored,too. This is solution");
+  		  		      		      	
+  		
+  		JLabel inspirationLabel = new JLabel("Who doesn't hate to watch a bad move twice? "
+ 				+ "If you are bored,too. This is solution!");
+  		inspirationLabel.setBackground(Color.white);
+  		inspirationLabel.setOpaque(true);
+  		inspirationLabel.setForeground(Color.pink);
+ 		rightPanel.add( inspirationLabel, "align center" );
+ 		inspirationLabel.setFont(new Font("Copperplate", Font.BOLD, 14 ));
     		
  		rightPanel.add(inspirationLabel);
+   
 
   		frame.pack();
- 		// Setting the frame visibility to true
  		frame.setVisible(true);
+ 	
  	}
 
   	}
